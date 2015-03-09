b27e9f444520623a07165115fb3439356ffc89df
Leonardo Lopez <itsshortforleo@hotmail.com>
2015-03-09 04:03:22 +0000

    p.193

--- Head First C Sharp (3rd Edition)/Chapter 04/ADayAtTheRaces/ADayAtTheRaces/Form1.Designer.cs
+++ Head First C Sharp (3rd Edition)/Chapter 04/ADayAtTheRaces/ADayAtTheRaces/Form1.Designer.cs
@@ -28,12 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.racetrackPictureBox = new System.Windows.Forms.PictureBox();
+            this.dog1PictureBox = new System.Windows.Forms.PictureBox();
+            this.dog2PictureBox = new System.Windows.Forms.PictureBox();
+            this.dog3PictureBox = new System.Windows.Forms.PictureBox();
+            this.dog4PictureBox = new System.Windows.Forms.PictureBox();
+            this.bettingParlorLabel = new System.Windows.Forms.Label();
+            this.minimumBetLabel = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.racetrackPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog1PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog2PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog3PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog4PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // racetrackPictureBox
+            // 
+            this.racetrackPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("racetrackPictureBox.Image")));
+            this.racetrackPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.racetrackPictureBox.Name = "racetrackPictureBox";
+            this.racetrackPictureBox.Size = new System.Drawing.Size(600, 200);
+            this.racetrackPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.racetrackPictureBox.TabIndex = 0;
+            this.racetrackPictureBox.TabStop = false;
+            // 
+            // dog1PictureBox
+            // 
+            this.dog1PictureBox.Image = ((System.Drawing.Image)(resources.GetObject("dog1PictureBox.Image")));
+            this.dog1PictureBox.Location = new System.Drawing.Point(12, 22);
+            this.dog1PictureBox.Name = "dog1PictureBox";
+            this.dog1PictureBox.Size = new System.Drawing.Size(75, 20);
+            this.dog1PictureBox.TabIndex = 1;
+            this.dog1PictureBox.TabStop = false;
+            // 
+            // dog2PictureBox
+            // 
+            this.dog2PictureBox.Image = ((System.Drawing.Image)(resources.GetObject("dog2PictureBox.Image")));
+            this.dog2PictureBox.Location = new System.Drawing.Point(12, 71);
+            this.dog2PictureBox.Name = "dog2PictureBox";
+            this.dog2PictureBox.Size = new System.Drawing.Size(75, 20);
+            this.dog2PictureBox.TabIndex = 2;
+            this.dog2PictureBox.TabStop = false;
+            // 
+            // dog3PictureBox
+            // 
+            this.dog3PictureBox.Image = ((System.Drawing.Image)(resources.GetObject("dog3PictureBox.Image")));
+            this.dog3PictureBox.Location = new System.Drawing.Point(12, 124);
+            this.dog3PictureBox.Name = "dog3PictureBox";
+            this.dog3PictureBox.Size = new System.Drawing.Size(75, 20);
+            this.dog3PictureBox.TabIndex = 3;
+            this.dog3PictureBox.TabStop = false;
+            // 
+            // dog4PictureBox
+            // 
+            this.dog4PictureBox.Image = ((System.Drawing.Image)(resources.GetObject("dog4PictureBox.Image")));
+            this.dog4PictureBox.Location = new System.Drawing.Point(12, 176);
+            this.dog4PictureBox.Name = "dog4PictureBox";
+            this.dog4PictureBox.Size = new System.Drawing.Size(75, 20);
+            this.dog4PictureBox.TabIndex = 4;
+            this.dog4PictureBox.TabStop = false;
+            // 
+            // bettingParlorLabel
+            // 
+            this.bettingParlorLabel.AutoSize = true;
+            this.bettingParlorLabel.Location = new System.Drawing.Point(9, 215);
+            this.bettingParlorLabel.Name = "bettingParlorLabel";
+            this.bettingParlorLabel.Size = new System.Drawing.Size(67, 13);
+            this.bettingParlorLabel.TabIndex = 5;
+            this.bettingParlorLabel.Text = "BettingParlor";
+            // 
+            // minimumBetLabel
+            // 
+            this.minimumBetLabel.AutoSize = true;
+            this.minimumBetLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimumBetLabel.Location = new System.Drawing.Point(12, 231);
+            this.minimumBetLabel.Name = "minimumBetLabel";
+            this.minimumBetLabel.Size = new System.Drawing.Size(104, 13);
+            this.minimumBetLabel.TabIndex = 6;
+            this.minimumBetLabel.Text = "minimumBetLabel";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 247);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(98, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "joeRadioButton";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 270);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(102, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.Text = "bobRadioButton";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 293);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(92, 17);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.Text = "alRadioButton";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(211, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Bets";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(211, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "joeBetLabel";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(211, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "bobBetLabel";
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(211, 293);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(189, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "alBetLabel";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(459, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 92);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Race!";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 325);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(625, 351);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.minimumBetLabel);
+            this.Controls.Add(this.bettingParlorLabel);
+            this.Controls.Add(this.dog4PictureBox);
+            this.Controls.Add(this.dog3PictureBox);
+            this.Controls.Add(this.dog2PictureBox);
+            this.Controls.Add(this.dog1PictureBox);
+            this.Controls.Add(this.racetrackPictureBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "A Day at the Races";
+            ((System.ComponentModel.ISupportInitialize)(this.racetrackPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog1PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog2PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog3PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dog4PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox racetrackPictureBox;
+        private System.Windows.Forms.PictureBox dog1PictureBox;
+        private System.Windows.Forms.PictureBox dog2PictureBox;
+        private System.Windows.Forms.PictureBox dog3PictureBox;
+        private System.Windows.Forms.PictureBox dog4PictureBox;
+        private System.Windows.Forms.Label bettingParlorLabel;
+        private System.Windows.Forms.Label minimumBetLabel;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
     }
 }
 
