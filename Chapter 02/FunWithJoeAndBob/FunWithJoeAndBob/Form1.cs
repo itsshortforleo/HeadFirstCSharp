64033b960d5c26dd2c3577c9f4ea4271f12023be
Leonardo Lopez <LLopez@rsimail.com>
2015-10-13 11:00:13 -0700

    Restructuring

---
+++ Chapter 02/FunWithJoeAndBob/FunWithJoeAndBob/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunWithJoeAndBob
+{
+    public partial class Form1 : Form
+    {
+        Guy joe;
+        Guy bob;
+        int bank = 100;
+        public void UpdateForm()
+        {
+            joesCashLabel.Text = joe.Name + " has $" + joe.Cash;
+            bobsCashLabel.Text = bob.Name + " has $" + bob.Cash;
+            bankCashLabel.Text = "The bank has $" + bank;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            // Initialize joe and bob here!
+            joe = new Guy() { Cash = 50, Name = "Joe" };
+            bob = new Guy() { Cash = 100, Name = "Bob" };
+            UpdateForm();
+        }
+
+        private void btnGive10ToJoe_Click(object sender, EventArgs e)
+        {
+            if (bank >= 10)
+            {
+                bank -= joe.ReceiveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("The bank is out of money.");
+            }
+        }
+
+        private void btnReceive5FromBob_Click(object sender, EventArgs e)
+        {
+            bank += bob.GiveCash(5);
+            UpdateForm();
+        }
+
+        private void joeGivesToBob_Click(object sender, EventArgs e)
+        {
+            if (joe.Cash>=10)
+            {
+                joe.Cash -= bob.ReceiveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("Joe doesn't have enough money to give to Bob.");
+            }
+        }
+
+        private void bobGivesToJoe_Click(object sender, EventArgs e)
+        {
+            if (bob.Cash>=5)
+            {
+                bob.Cash -= joe.ReceiveCash(5);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("Bob doesn't have enough money to give to Joe.");
+               
+            }
+        }
+    }
+}
---
+++ Chapter 04/ADayAtTheRaces/ADayAtTheRaces/Greyhound.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADayAtTheRaces
+{
+    public class Greyhound
+    {
+        public int StartingPosition; // Where my PictureBox starts
+        public int RacetrackLength; // How long the racetrack is
+        public PictureBox MyPictureBox = null; // My PictureBox object
+        public int Location = 0; // My Location on the racetrack
+        public Random Randomizer; // An instance of Random
+        public bool PulledHamstring = false;
+        public int PulledHamstringCounter = 0;
+        
+        public bool Run()
+        {
+            // Move forward either 1, 2, 3 or 4 spaces at random
+            // Update the position of my PictureBox on the form like this:
+            //      MyPictureBox.Left = StartingPosition + Location;
+            // Return true if I won the race
+            Location=Randomizer.Next(9);
+            if (Location>=4)
+            {
+                // Pulled hamstring
+                PulledHamstring = true;
+
+                MyPictureBox.Left = MyPictureBox.Left + StartingPosition;
+                PulledHamstringCounter++;
+            }
+            if (PulledHamstring==true && PulledHamstringCounter<=3)
+            {
+                MyPictureBox.Left = MyPictureBox.Left + StartingPosition;
+                PulledHamstringCounter++;
+            }
+            else
+            {
+                PulledHamstring = false;
+                MyPictureBox.Left = MyPictureBox.Left + StartingPosition + Location;
+            }
+            
+            if (MyPictureBox.Left>=RacetrackLength)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        public void TakeStartingPosition()
+        { 
+            // Reset my location to 0 and PictureBox to starting position
+            Location = 0;
+            MyPictureBox.Left = StartingPosition;
+        }
+
+    }
+}
