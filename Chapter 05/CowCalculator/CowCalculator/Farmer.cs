e90b02af3a29056331c4003072bb6b12d63f7914
Leonardo Lopez <LLopez@rsimail.com>
2015-10-13 12:31:45 -0700

    Incremental check in

---
+++ Chapter 05/CowCalculator/CowCalculator/Farmer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Head_First_C_Sharp.Chapter_05.CowCalculator.CowCalculator
+{
+    class Farmer
+    {
+        public int BagsOfFeed;
+        public const int FeedMultiplier = 30;
+
+        private int numberOfCows;
+
+        public int NumberOfCows
+        {
+            get { return numberOfCows; }
+            set 
+            {
+                numberOfCows = value;
+                BagsOfFeed = numberOfCows * FeedMultiplier;
+            }
+        }
+        
+    }
+}
