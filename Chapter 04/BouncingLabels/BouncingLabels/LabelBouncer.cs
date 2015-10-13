4b9ffd0716b268b861ba50934bf1312f432beedc
Leonardo Lopez <LLopez@rsimail.com>
2015-10-13 11:03:33 -0700

    Folder structure

---
+++ Chapter 04/BouncingLabels/BouncingLabels/LabelBouncer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingLabels
+{
+    class LabelBouncer
+    {
+        public Label MyLabel;
+        public bool GoingFoward = true;
+
+        public void Move()
+        {
+            if (MyLabel != null)
+            {
+                if (GoingFoward == true)
+                {
+                    MyLabel.Left += 5;
+                    if (MyLabel.Left >= MyLabel.Parent.Width - MyLabel.Width)
+                    {
+                        GoingFoward = false;
+                    }
+                }
+                else
+                {
+                    MyLabel.Left -= 5;
+                    if (MyLabel.Left <= 0)
+                    {
+                        GoingFoward = true;
+                    }
+                }
+            }
+	    }
+    }
+}
---
+++ Chapter 05/PartyPlanner/PartyPlanner/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace PartyPlanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumberOfPeople = new System.Windows.Forms.Label();
+            this.nudNumberOfPeople = new System.Windows.Forms.NumericUpDown();
+            this.fancyBox = new System.Windows.Forms.CheckBox();
+            this.healthyBox = new System.Windows.Forms.CheckBox();
+            this.lblCost = new System.Windows.Forms.Label();
+            this.costLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfPeople)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNumberOfPeople
+            // 
+            this.lblNumberOfPeople.AutoSize = true;
+            this.lblNumberOfPeople.Location = new System.Drawing.Point(13, 13);
+            this.lblNumberOfPeople.Name = "lblNumberOfPeople";
+            this.lblNumberOfPeople.Size = new System.Drawing.Size(92, 13);
+            this.lblNumberOfPeople.TabIndex = 0;
+            this.lblNumberOfPeople.Text = "Number of People";
+            // 
+            // nudNumberOfPeople
+            // 
+            this.nudNumberOfPeople.Location = new System.Drawing.Point(13, 30);
+            this.nudNumberOfPeople.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nudNumberOfPeople.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudNumberOfPeople.Name = "nudNumberOfPeople";
+            this.nudNumberOfPeople.Size = new System.Drawing.Size(92, 20);
+            this.nudNumberOfPeople.TabIndex = 1;
+            this.nudNumberOfPeople.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudNumberOfPeople.ValueChanged += new System.EventHandler(this.nudNumberOfPeople_ValueChanged);
+            // 
+            // fancyBox
+            // 
+            this.fancyBox.AutoSize = true;
+            this.fancyBox.Checked = true;
+            this.fancyBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.fancyBox.Location = new System.Drawing.Point(13, 57);
+            this.fancyBox.Name = "fancyBox";
+            this.fancyBox.Size = new System.Drawing.Size(115, 17);
+            this.fancyBox.TabIndex = 2;
+            this.fancyBox.Text = "Fancy Decorations";
+            this.fancyBox.UseVisualStyleBackColor = true;
+            this.fancyBox.CheckedChanged += new System.EventHandler(this.fancyBox_CheckedChanged);
+            // 
+            // healthyBox
+            // 
+            this.healthyBox.AutoSize = true;
+            this.healthyBox.Location = new System.Drawing.Point(13, 80);
+            this.healthyBox.Name = "healthyBox";
+            this.healthyBox.Size = new System.Drawing.Size(96, 17);
+            this.healthyBox.TabIndex = 3;
+            this.healthyBox.Text = "Healthy Option";
+            this.healthyBox.UseVisualStyleBackColor = true;
+            this.healthyBox.CheckedChanged += new System.EventHandler(this.healthyBox_CheckedChanged);
+            // 
+            // lblCost
+            // 
+            this.lblCost.AutoSize = true;
+            this.lblCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCost.Location = new System.Drawing.Point(11, 108);
+            this.lblCost.Name = "lblCost";
+            this.lblCost.Size = new System.Drawing.Size(32, 13);
+            this.lblCost.TabIndex = 4;
+            this.lblCost.Text = "Cost";
+            // 
+            // costLabel
+            // 
+            this.costLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.costLabel.Location = new System.Drawing.Point(49, 108);
+            this.costLabel.Name = "costLabel";
+            this.costLabel.Size = new System.Drawing.Size(66, 16);
+            this.costLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(148, 133);
+            this.Controls.Add(this.costLabel);
+            this.Controls.Add(this.lblCost);
+            this.Controls.Add(this.healthyBox);
+            this.Controls.Add(this.fancyBox);
+            this.Controls.Add(this.nudNumberOfPeople);
+            this.Controls.Add(this.lblNumberOfPeople);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Party Planner";
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfPeople)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumberOfPeople;
+        private System.Windows.Forms.NumericUpDown nudNumberOfPeople;
+        private System.Windows.Forms.CheckBox fancyBox;
+        private System.Windows.Forms.CheckBox healthyBox;
+        private System.Windows.Forms.Label lblCost;
+        private System.Windows.Forms.Label costLabel;
+    }
+}
+
