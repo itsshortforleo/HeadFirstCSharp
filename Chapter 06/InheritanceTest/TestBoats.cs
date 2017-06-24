5229f01bcb5f2d51a3459814c26f16a4bd7d1262
Leonardo Lopez <llopez@rsimail.com>
2017-06-24 14:59:56 -0700

    InheritanceTest project check in

---
+++ Chapter 06/InheritanceTest/TestBoats.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InheritanceTest
+{
+    static class TestBoats
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        //[STAThread]
+        public static void Main()
+        {
+            string xyz = "";
+            Boat b1 = new Boat();
+            Sailboat b2 = new Sailboat();
+            Rowboat b3 = new Rowboat();
+            b2.SetLength(32);
+            xyz = b1.move();
+            xyz += b3.move();
+            xyz += b2.move();
+            System.Windows.Forms.MessageBox.Show(xyz);
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new InheritanceTestForm());
+        }
+    }
+}
