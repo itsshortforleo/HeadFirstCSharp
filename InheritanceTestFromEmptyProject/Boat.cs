f37b4ed77fab968e02f24c65664d6fd58e0a259e
Leonardo Lopez <llopez@rsimail.com>
2017-06-25 21:55:34 -0700

    More practice programs

---
+++ InheritanceTestFromEmptyProject/Boat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceTest
+{
+    class Boat
+    {
+        private int length;
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public void SetLength(int len)
+        {
+            length = len;
+        }
+        public virtual string move()
+        {
+            return "drift ";
+        }
+    }
+}
---
+++ InheritanceTestFromEmptyProject/TestBoats.cs
@@ -0,0 +1,26 @@
+﻿namespace InheritanceTest
+{
+    static class TestBoats
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        //[STAThread]
+        public static void Main()
+        {
+            string xyz = "";
+            Boat b1 = new Boat();
+            Sailboat b2 = new Sailboat();
+            Rowboat b3 = new Rowboat();
+            b2.SetLength(32);
+            xyz = b1.move();
+            xyz += b3.move();
+            xyz += b2.move();
+            System.Windows.Forms.MessageBox.Show(xyz);
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new InheritanceTestForm());
+        }
+    }
+}
